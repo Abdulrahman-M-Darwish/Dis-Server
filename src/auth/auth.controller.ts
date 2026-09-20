6654db2c7435022488332e5b5e7952d9b60c45ef
Abdulrahman-M-Darwish <winding192837456456@gmail.com>
2026-09-20 15:53:10 +0300

    fix cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -42,6 +42,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return { accessToken };
